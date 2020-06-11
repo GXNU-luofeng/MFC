81cd77ce481d44e13bf4da696f0a07f62cc4071c
GXNU-luofeng <1441834346@qq.com>
2020-06-12 01:51:32 +0800

    2020/6/12推送
    Signed-off-by: GXNU-luofeng <1441834346@qq.com>

---
+++ 画以随机数为长半轴和短半轴的椭圆文档类/画以随机数为长半轴和短半轴的椭圆文档类/画以随机数为长半轴和短半轴的椭圆文档类Doc.h
@@ -0,0 +1,49 @@
+
+// 画以随机数为长半轴和短半轴的椭圆文档类Doc.h : C画以随机数为长半轴和短半轴的椭圆文档类Doc 类的接口
+//
+
+
+#pragma once
+
+
+class C画以随机数为长半轴和短半轴的椭圆文档类Doc : public CDocument
+{
+protected: // 仅从序列化创建
+	C画以随机数为长半轴和短半轴的椭圆文档类Doc();
+	DECLARE_DYNCREATE(C画以随机数为长半轴和短半轴的椭圆文档类Doc)
+
+// 特性
+public:
+	CArray<CRect, CRect&> ca;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~C画以随机数为长半轴和短半轴的椭圆文档类Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
